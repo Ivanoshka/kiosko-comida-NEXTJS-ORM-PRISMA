ccbb2c4f3060d4b5d2706cce3f02ded51d06318c
Ivanoshka <19030188@itcelaya.edu.mx>
2024-06-04 03:27:20 -0600

    Conecte a la base de datos Postgresql y añadi los datos de categorias y productos a la base de datos

---
+++ prisma/migrations/20240604084823_categories_migration/migration.sql
@@ -0,0 +1,10 @@
+-- CreateTable
+CREATE TABLE "Category" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "slug" TEXT NOT NULL,
+
+    CONSTRAINT "Category_pkey" PRIMARY KEY ("id")
+);
+
+
